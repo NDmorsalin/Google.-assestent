4730accc65874b6f0961ab3d93a702fd1d78abca
NDmorsalin <morsalin056@gmail.com>
2020-02-02 23:14:48 +0600

    add

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
+var ndmMath={
+    morsalin:function(base,power){
+        var arr=[],result;
+        for(var i=0;i<power;i++){
+            arr[i]=base;
+           var res=1;
+            for(j=0;j<arr.length;j++){ 
+            res=res*arr[j];
+            }
+        }
+      return res;
+        
+      }
+    };
+    console.log(Math.pow(10,2));
+   console.log(ndmMath.morsalin(2,3));
 
-console.log("Hello World");
+
+
